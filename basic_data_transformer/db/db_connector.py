1ae0998fd8aa63e2aa96b0dd692fa16e1e59d2b6
Michal Raczycki <michalr265@gmail.com>
2023-07-30 18:49:11 +0200

    adding the rest of the basic CRUD

--- basic_data_transformer/db/db_connector.py
+++ basic_data_transformer/db/db_connector.py
@@ -9,3 +9,26 @@ class DatabaseConnector:
     def get_session(self):
         return self.Session()
 
+    def select_data(self, table_name, condition=None):
+            session = self.get_session()
+            query = f"SELECT * FROM {table_name}"
+            if condition:
+                query += f" WHERE {condition}"
+            result = session.execute(query)
+            data = result.fetchall()
+            session.close()
+            return data
+    
+    def insert_data(self, table_name, data):
+        session = self.get_session()
+        query = f"INSERT INTO {table_name} VALUES {data}"
+        session.execute(query)
+        session.commit()
+        session.close()
+
+    def update_data(self, table_name, data, condition):
+        session = self.get_session()
+        query = f"UPDATE {table_name} SET {data} WHERE {condition}"
+        session.execute(query)
+        session.commit()
+        session.close()
