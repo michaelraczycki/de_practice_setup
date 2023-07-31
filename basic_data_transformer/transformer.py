f9dc5aca2b0a54bb6f28e2eba9f8fc3f21ed6bf8
Michal Raczycki <michalr265@gmail.com>
2023-07-31 08:48:04 +0200

    adding data files

--- basic_data_transformer/transformer.py
+++ basic_data_transformer/transformer.py
@@ -1,4 +1,9 @@
 import pandas as pd
-from .db.db_connector import DatabaseConnector
+from db.db_connector import DatabaseConnector
+import glob
 
 db = DatabaseConnector('root', 'root', 'localhost', '5432', 'postgres')
+session = db.get_session()
+session.close()
+
+help(glob.glob)
